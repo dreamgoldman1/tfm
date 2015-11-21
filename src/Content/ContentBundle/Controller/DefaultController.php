153c4a9a9d5fdc6dfd2d050069c6a52724e6c7e7
Gilberto Orozco <gilbertgol@gmail.com>
2015-11-20 19:09:33 -0500

    Se realiza el desarrollo de contenidos para el home y el perfil de usuarios

--- src/Content/ContentBundle/Controller/DefaultController.php
+++ src/Content/ContentBundle/Controller/DefaultController.php
@@ -10,4 +10,60 @@ class DefaultController extends Controller
     {
         return $this->render('ContentContentBundle:Default:index.html.twig');
     }
+    
+    public function getCuidadorDataAction($idCuidador)
+    {
+        switch ($idCuidador){
+            
+            case 1:
+                $fotos = array('1.jpg', '2.jpg', '3.jpg');
+                $tipoMascota = array(
+                    'grande' => 'Si',
+                    'mediano' => 'Si',
+                    'pequeño' => 'Si',
+                    );
+                $data = array(
+                    'fotoPerfil' => 'perfil-hombre.png',
+                    'nombre' => 'Gilberto Orozco Linero',
+                    'perfil' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                    'tipoMascota' => $tipoMascota,
+                    'alimentacion' => 'Si',
+                    'paseo' => 'Si',
+                    'recogidaDomicilio' => 'Si',
+                    'genero' => 'Masculino',
+                    'reputacion' => '1 2 3 <strong>4</strong> 5',
+                    'comentarios' => '',
+                    'fotos' => $fotos,
+                    'tarifaPorDia' => '70000',
+                    'tarifaPorHora' => '10000',
+                    'calendario' => '',
+                );
+                break;
+            case 2:
+                $fotos = array('1.jpg', '2.jpg');
+                $tipoMascota = array(
+                    'grande' => 'No',
+                    'mediano' => 'Si',
+                    'pequeño' => 'Si',
+                    );
+                $data = array(
+                    'fotoPerfil' => 'perfil-mujer.png',
+                    'nombre' => 'Wendy Solano',
+                    'perfil' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                    'tipoMascota' => $tipoMascota,
+                    'alimentacion' => 'Si',
+                    'paseo' => 'No',
+                    'recogidaDomicilio' => 'No',
+                    'genero' => 'Femenino',
+                    'reputacion' => '1 2 3 <strong>4</strong> 5',
+                    'comentarios' => '',
+                    'fotos' => $fotos,
+                    'tarifaPorDia' => '70000',
+                    'tarifaPorHora' => '10000',
+                    'calendario' => '',
+                );
+                break;
+        }
+        return $this->render('ContentContentBundle:Default:informacion-cuidador.html.twig', $data);
+    }
 }
